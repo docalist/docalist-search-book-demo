6b1b18f34abe4e80e6bf3c139f3bf174b7328f2b
Daniel Ménard <daniel.menard@laposte.net>
2015-10-28 21:06:08 +0100

    Plugin : plusieurs corrections - corrige le text-domain (inversion book / demo) - inutile de réécrire view(), on peut utiliser docalist('views') - typos.

--- class/Plugin.php
+++ class/Plugin.php
@@ -12,10 +12,8 @@
  */
 namespace Docalist\BookDemo;
 
-use Exception;
-
 /**
- * Class principale du plugin.
+ * Classe principale du plugin.
  */
 class Plugin
 {
@@ -35,7 +33,7 @@ class Plugin
     public function __construct()
     {
         // Charge les fichiers de langue du plugin
-        load_plugin_textdomain('docalist-search-demo-book', false, 'docalist-search-demo-book/languages');
+        load_plugin_textdomain('docalist-search-book-demo', false, 'docalist-search-book-demo/languages');
 
         // Crée le type "livre" et la taxonomie associée "types de livre"
         add_action('init', function () {
@@ -59,12 +57,12 @@ class Plugin
         add_action('admin_menu', function () {
             add_submenu_page(
                 'edit.php?post_type=bookdemo',
-                __("Test de l'API de recherche docalist-search", 'docalist-search-demo-book/languages'),
-                __('API SearchRequest', 'docalist-search-demo-book/languages'),
+                __("Test de l'API de recherche docalist-search", 'docalist-search-book-demo/languages'),
+                __('API SearchRequest', 'docalist-search-book-demo/languages'),
                 'manage_options',
                 'docalist-book-search-request',
                 function () {
-                    $this->view('searchrequest');
+                    docalist('views')->display('docalist-search-book-demo:searchrequest');
                 }
             );
         });
@@ -77,21 +75,21 @@ class Plugin
     {
         register_taxonomy(self::TAXONOMY, [], [
             'labels' => [
-                'name' => __('Types de livres', 'docalist-search-demo-book'),
-                'singular_name' => __('Type de livre', 'docalist-search-demo-book'),
-                'menu_name' => __('Types de livres', 'docalist-search-demo-book'),
-                'all_items' => __('Tous les types', 'docalist-search-demo-book'),
-                'new_item_name' => __('Nouveau type', 'docalist-search-demo-book'),
-                'add_new_item' => __('Ajouter un type', 'docalist-search-demo-book'),
-                'edit_item' => __('Modifier le type', 'docalist-search-demo-book'),
-                'update_item' => __('Enregistrer', 'docalist-search-demo-book'),
-                'view_item' => __('Voir le type', 'docalist-search-demo-book'),
-                'separate_items_with_commas' => __('Séparez les types avec une virgule', 'docalist-search-demo-book'),
-                'add_or_remove_items' => __('Ajouter ou supprimer des types', 'docalist-search-demo-book'),
-                'choose_from_most_used' => __('Choisissez parmi plus souvent utilisés', 'docalist-search-demo-book'),
-                'popular_items' => __('Types courants', 'docalist-search-demo-book'),
-                'search_items' => __('Rechercher un type', 'docalist-search-demo-book'),
-                'not_found' => __('Non trouvé', 'docalist-search-demo-book'),
+                'name' => __('Types de livres', 'docalist-search-book-demo'),
+                'singular_name' => __('Type de livre', 'docalist-search-book-demo'),
+                'menu_name' => __('Types de livres', 'docalist-search-book-demo'),
+                'all_items' => __('Tous les types', 'docalist-search-book-demo'),
+                'new_item_name' => __('Nouveau type', 'docalist-search-book-demo'),
+                'add_new_item' => __('Ajouter un type', 'docalist-search-book-demo'),
+                'edit_item' => __('Modifier le type', 'docalist-search-book-demo'),
+                'update_item' => __('Enregistrer', 'docalist-search-book-demo'),
+                'view_item' => __('Voir le type', 'docalist-search-book-demo'),
+                'separate_items_with_commas' => __('Séparez les types avec une virgule', 'docalist-search-book-demo'),
+                'add_or_remove_items' => __('Ajouter ou supprimer des types', 'docalist-search-book-demo'),
+                'choose_from_most_used' => __('Choisissez parmi plus souvent utilisés', 'docalist-search-book-demo'),
+                'popular_items' => __('Types courants', 'docalist-search-book-demo'),
+                'search_items' => __('Rechercher un type', 'docalist-search-book-demo'),
+                'not_found' => __('Non trouvé', 'docalist-search-book-demo'),
 
             ],
             'hierarchical' => false,
@@ -115,53 +113,18 @@ class Plugin
             'menu_icon' => 'dashicons-book-alt',
             'show_in_nav_menus' => false,
             'labels' => [
-                'name' => _x('Livres', 'Post Type General Name', 'docalist-search-demo-book'),
-                'singular_name' => _x('Livre', 'Post Type Singular Name', 'docalist-search-demo-book'),
-                'menu_name' => __('Livres (demo)', 'docalist-search-demo-book'),
-                'name_admin_bar' => __('Livre (demo)', 'docalist-search-demo-book'),
-                'all_items' => __('Liste des livres', 'docalist-search-demo-book'),
-                'add_new_item' => __('Ajouter un livre', 'docalist-search-demo-book'),
-                'add_new' => __('Ajouter un livre', 'docalist-search-demo-book'),
-                'new_item' => __('Nouveau livre', 'docalist-search-demo-book'),
-                'edit_item' => __('Modifier le livre', 'docalist-search-demo-book'),
-                'update_item' => __('Enregistrer', 'docalist-search-demo-book'),
-                'view_item' => __('Voir le livre', 'docalist-search-demo-book'),
+                'name' => _x('Livres', 'Post Type General Name', 'docalist-search-book-demo'),
+                'singular_name' => _x('Livre', 'Post Type Singular Name', 'docalist-search-book-demo'),
+                'menu_name' => __('Livres (demo)', 'docalist-search-book-demo'),
+                'name_admin_bar' => __('Livre (demo)', 'docalist-search-book-demo'),
+                'all_items' => __('Liste des livres', 'docalist-search-book-demo'),
+                'add_new_item' => __('Ajouter un livre', 'docalist-search-book-demo'),
+                'add_new' => __('Ajouter un livre', 'docalist-search-book-demo'),
+                'new_item' => __('Nouveau livre', 'docalist-search-book-demo'),
+                'edit_item' => __('Modifier le livre', 'docalist-search-book-demo'),
+                'update_item' => __('Enregistrer', 'docalist-search-book-demo'),
+                'view_item' => __('Voir le livre', 'docalist-search-book-demo'),
             ],
         ]);
-    }
-
-    /**
-     * Affiche une vue (un template).
-     *
-     * @param string $view Nom de la vue à afficher (dans le répertoire 'views' du plugin,
-     * ne pas indiquer l'extension '.php' qui est ajoutée automatiquement).
-     * @param array $data données à transmettre à la vue
-     *
-     * @throws Exception
-     */
-    public function view($view, array $data = [])
-    {
-        // Détermine le path exact de la vue
-        $path = __DIR__ . '/../views/' . $view . '.php';
-        if (! is_readable($path)) {
-            $msg = __('Vue non trouvée "%s"', 'docalist-search-demo-book');
-            throw new Exception(sprintf($msg, $view));
-        }
-
-        // Crée la closure qui exécute le template (sandbox)
-        $render = function (array $view) {
-            extract($view, EXTR_OVERWRITE | EXTR_REFS);
-
-            return require $view['path'];
-        };
-
-        $data['view'] = ['name' => $view, 'path' => $path, 'data' => $data];
-
-        // Binde la closure pour que $this soit dispo dans la vue
-        $context = isset($data['this']) ? $data['this'] : null;
-        $render = $render->bindTo($context, $context);
-
-        // Exécute le template
-        return $render($data);
     }
 }
